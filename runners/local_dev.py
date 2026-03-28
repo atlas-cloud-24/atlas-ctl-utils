fc3566c43d653792ef429a62c2c10705c484c63b
atlas-cloud24 <atlas.cloud24@gmail.com>
2026-03-28 20:38:54 +0200

    28.03.2026 upd

--- runners/local_dev.py
+++ runners/local_dev.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
-from local_common import run_local_runner
-
+from utils.stage_local_runner import run_local_runner
 
 if __name__ == "__main__":
     run_local_runner("local_dev.sh")
